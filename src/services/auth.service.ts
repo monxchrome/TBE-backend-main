0a6e9385500e7e440033a5b0d6e6a9f487da605e
monxchrome <inacheat@gmail.com>
2025-02-25 12:03:25 +0100

    Added login logic

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,46 @@
+import bcrypt from "bcrypt";
+
+import { ApiError } from "../errors/index.js";
+import { User } from "../models/User.model.js";
+import { ICredentials } from "../types/auth.types.js";
+import { ITokenPair } from "../types/token.types.js";
 import { IUser } from "../types/user.types.js";
+import { oauthService } from "./oauth.service.js";
 
 class AuthService {
   public async register(body: IUser) {
-    const { password } = body;
+    try {
+      const { password } = body;
+
+      const hashedPassword = await oauthService.hash(password);
+      await User.create({
+        ...body,
+        password: hashedPassword,
+      });
+    } catch (e) {
+      if (e instanceof ApiError) {
+        throw e;
+      } else if (e instanceof Error) {
+        throw new ApiError(e.message, 500);
+      } else {
+        throw new ApiError("Unknown error", 500);
+      }
+    }
+  }
+
+  public async login(
+    credentials: ICredentials,
+    user: IUser,
+  ): Promise<ITokenPair> {
+    try {
+      const isMatched = oauthService.compare(
+        credentials.password,
+        user.password,
+      );
+
+      if (!isMatched) {
+        throw new ApiError("Email or password incorrect!", 400);
+      }
+    } catch (e) {}
   }
 }
